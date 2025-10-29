0c374a6a748e046664637f422881cc16ecf06e62
cst_jaqueline.santos <cst_jaqueline.santos@minervafoods.com>
2025-10-29 19:26:01 -0300

    Atualizado migration

--- src/BugStore.Infrastructure/Migrations/20251029222429_v1.Designer.cs
+++ src/BugStore.Infrastructure/Migrations/20251029222429_v1.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BugStore.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20251029214447_v1")]
+    [Migration("20251029222429_v1")]
     partial class v1
     {
         /// <inheritdoc />
